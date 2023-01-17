fa751147ad228390253fe3915ab86062ea7350a5
Denys Parshutkin <ddenchayy@gmail.com>
2023-01-17 22:17:07 +0200

    learned logic blocs

---
+++ src/by/epam/learn/logic/DemoLogicMain.java
@@ -0,0 +1,8 @@
+package by.epam.learn.logic;
+
+public class DemoLogicMain {
+    public static void main(String[] args) {
+        new Department(71);
+        new Department(17);
+    }
+}
--- src/by/epam/learn/logic/Department.java
+++ src/by/epam/learn/logic/Department.java
@@ -4,6 +4,18 @@ public class Department {
     {
         System.out.println("logic (1) id=" + this.id);
     }
-    static
-
+    static{
+        System.out.println("static logic");
+    }
+    private int id = 42;
+    public Department(int id){
+        this.id = id;
+        System.out.println("constructor id=" + id);
+    }
+    public int getId(){
+        return id;
+    }
+    {
+        System.out.println("logic (2) id=" + id);
+    }
 }
